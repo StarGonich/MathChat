f1b2e1ec2a937c708ac099ddc0038b43fdce413a
Alexey Velichayshiy <alexey8837@gmail.com>
2025-09-04 18:44:48 +0300

    init commit

---
+++ src/test/java/ru/sber/practice/MathChat/MathChatApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sber.practice.MathChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
