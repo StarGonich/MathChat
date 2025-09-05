a6401bf08cfe7fd5fe3055e16d1d2db5c14a5bc0
Alexey Velichayshiy <alexey8837@gmail.com>
2025-09-05 20:23:10 +0300

    monorepo fix

---
+++ backend/src/main/java/ru/sber/practice/MathChat/MathChatApplication.java
@@ -0,0 +1,13 @@
+package ru.sber.practice.MathChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathChatApplication.class, args);
+	}
+
+}
