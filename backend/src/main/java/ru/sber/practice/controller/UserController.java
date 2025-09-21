1a568edfb1f8b441ed767254c8c54ba28de51a33
Alexey Velichayshiy <alexey8837@gmail.com>
2025-09-21 18:42:04 +0300

    /apu/user, для стандартизации

--- backend/src/main/java/ru/sber/practice/controller/UserController.java
+++ backend/src/main/java/ru/sber/practice/controller/UserController.java
@@ -1,7 +1,6 @@
 package ru.sber.practice.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +10,7 @@ import ru.sber.practice.service.UserService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 @AllArgsConstructor
 public class UserController {
     private final UserService userService;
