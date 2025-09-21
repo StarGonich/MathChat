aefe9efabdc89d8afa2aeb650c8d3171d5bdde5a
Alexey Velichayshiy <alexey8837@gmail.com>
2025-09-21 17:55:41 +0300

    Создан MessageEntity

--- backend/src/main/java/ru/sber/practice/model/Message.java
+++ backend/src/main/java/ru/sber/practice/model/Message.java
@@ -1,4 +1,32 @@
 package ru.sber.practice.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "messages")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, name = "user_id")
+    private String userId;
+
+    @Column(nullable = false, name = "user_id")
+    private String chatId;
+
+    @Column(nullable = false, name = "message_text")
+    private String messageText;
+
+    @CreationTimestamp
+    private ZonedDateTime date;
 }
