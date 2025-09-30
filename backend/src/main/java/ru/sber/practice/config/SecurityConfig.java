f8e08d356bfc95152d8f275b1ad6c22cb6977963
MatveyKov11 <159719009+MatveyKov11@users.noreply.github.com>
2025-09-30 13:39:39 +0300

    Update SecurityConfig.java
    Откат настройки cors и настройки порта

--- backend/src/main/java/ru/sber/practice/config/SecurityConfig.java
+++ backend/src/main/java/ru/sber/practice/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig{
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
+        configuration.setAllowedOrigins(List.of("http://localhost:9000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
@@ -46,7 +46,7 @@ public class SecurityConfig{
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .cors(Customizer.withDefaults()) // Настройка CORS
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 /*.authorizeHttpRequests(auth -> auth
                         .requestMatchers("/register", "/user/**", "/activate/*").permitAll()
