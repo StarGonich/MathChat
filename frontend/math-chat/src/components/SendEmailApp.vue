5ea0e50b3ed08a36d1d93a8ac58051e0c37bc257
Matvey Kovalev <matvey.kovalev@ispras.ru>
2025-10-05 21:01:45 +0300

    Добавление страницы подтверждения почты с таймером (без привязки к бэку)

---
+++ frontend/math-chat/src/components/SendEmailApp.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="ui text container">
+        <h2 class="ui center header">Подтверждение почты</h2>
+        <form class="ui raised center aligned container segment">
+            <div class="ui message">
+                На вашу почту пришло письмо! Для подтверждения откройте ссылку в письме.
+            </div>
+            <h2 class="ui center header">{{ formatTime(time) }}</h2>
+            <div class="ui clearing divider"></div>
+            <button class = "ui button primary" @click="post" :disabled="time > 0">Отправить письмо еще раз</button>
+        </form>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+let time = ref(150)
+
+function formatTime(t){
+    let s = ''
+    s += Math.floor(t/60) + ':'
+    if (t % 60 < 10){
+        s += '0'
+    }
+    s += t%60
+    return s 
+}
+
+onMounted(() => {
+    setTimeout(tick, 1000)
+})
+
+function tick() {
+    if (time.value > 0){
+        time.value--
+        setTimeout(tick, 1000)
+    }
+}
+
+function post(){
+    time.value = 150
+    tick()
+}
+</script>
