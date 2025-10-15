0bde68aab710630bf69548dad51672870bd07a90
Matvey Kovalev <matvey.kovalev@ispras.ru>
2025-10-15 13:37:19 +0300

    Улучшение дизайна страницы входа

--- frontend/math-chat/src/components/AuthApp.vue
+++ frontend/math-chat/src/components/AuthApp.vue
@@ -1,46 +1,61 @@
 <template>
     <div class="ui text container">
-        <h2 class="ui center header">Вход</h2>
-        <form class="ui raised left aligned container segment">
-            <div>
-                <p><label>Почта: </label></p>
-                <div class="ui input">
-                    <input v-model="user.email" place-holder="Почта">
-                </div>
-                <div class="ui left pointing red basic  label" v-if="!user.email">
-                    Нет почты
-                </div>
+        <h2 class="ui image header">
+            <img src="./static/images/user.png" class="image">
+            <div class="content">
+                Вход
             </div>
-            <div class="ui clearing divider"></div>
-            <div>
-                <p><label>Пароль: </label></p>
-                <div class="ui input">
-                    <input v-model="user.password" place-holder="Пароль" type="password" @input="onInputPass">
+        </h2>
+        <div class="ui raised left aligned container segment">
+            <form class="ui large form">
+                <div class="field">
+                    <div :class="emailClass">
+                        <i class="user icon"></i>
+                        <input type="text" name="email" v-model="user.email" placeholder="Почта">
+                    </div>
                 </div>
-                <div class="ui left pointing red basic  label" v-if="!user.password">
-                    Нет пароля
+                <div class="ui clearing divider"></div>
+                <div class="field">
+                    <div :class="passClass">
+                        <i class="lock icon"></i>
+                        <input type="password" name="password" v-model="user.password" placeholder="Пароль">
+                    </div>
                 </div>
-            </div>
-            <div class="ui clearing divider"></div>
-            <button class = "ui button primary" @click="auth" :disabled="!user.email || !user.password">Войти</button>
-            <button class = "ui button" @click="register">Нет аккаунта? Зарегистрироваться</button>
-            <button class = "ui button" @click.prevent="forgot" :disabled="!user.email">Забыли пароль?</button>
-            <div class="ui info message" v-if="msg">
-                <i class="close icon" @click="close"></i>
-                <div class="header">
-                    {{ msg }}
+                <div class="ui clearing divider"></div>
+                <button class = "ui button primary" @click="auth" :disabled="!user.email || !user.password">Войти</button>
+                <button class = "ui button" @click="register">Нет аккаунта? Зарегистрироваться</button>
+                <button class = "ui button" @click.prevent="forgot" :disabled="!user.email">Забыли пароль?</button>
+                <div class="ui info message" v-if="msg">
+                    <i class="close icon" @click="close"></i>
+                    <div class="header">
+                        {{ msg }}
+                    </div>
                 </div>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
 </template>
 
 <script setup>
 import axios from 'axios'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 const user = ref({
     email: '',
     password: ''
+})
+const emailClass = ref('ui left icon input')
+const passClass = ref('ui left icon input')
+watch(user.value, (newUser) =>{
+    if (newUser.email){
+        emailClass.value = 'ui left icon input'
+    }else{
+        emailClass.value = 'ui left icon error input'
+    }
+    if (newUser.password){
+        passClass.value = 'ui left icon input'
+    }else{
+        passClass.value = 'ui left icon error input'
+    }
 })
 const emit = defineEmits(['quitEvent'])
 const msg=ref('')
